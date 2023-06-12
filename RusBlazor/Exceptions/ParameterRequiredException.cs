a59f6f8ddc29594a5288bdca655f623ab9365887
Marius <marius@fjeld.et>
2023-06-12 13:40:47 +0200

    Init Tests

---
+++ RusBlazor/Exceptions/ParameterRequiredException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RusBlazor.Exceptions
+{
+    public class ParameterRequiredException : Exception
+    {
+        public ParameterRequiredException(string parameterName)
+                    : base($"The required parameter {parameterName} is missing.")
+        {
+
+        }
+    }
+}
