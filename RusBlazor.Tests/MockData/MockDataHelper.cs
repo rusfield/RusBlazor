bf2b5ee545472c48d3d535291e9f31398952870d
Marius <marius@fjeld.et>
2023-06-13 12:33:53 +0200

    Tests and updates for RusSelect with relateds

---
+++ RusBlazor.Tests/MockData/MockDataHelper.cs
@@ -0,0 +1,26 @@
+﻿namespace RusBlazor.Tests.MockData
+{
+    public static class MockDataHelper
+    {
+        public static SortedDictionary<int, string> BuildDummyData(int start, int amount, bool flags)
+        {
+            var results = new SortedDictionary<int, string>();
+            for(int i = start; i < amount;)
+            {
+                results.Add(i, $"Value {i}");
+                if (flags)
+                {
+                    if (i == 0)
+                        i = 1;
+                    else
+                        i *= 2;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            return results;
+        }
+    }
+}
