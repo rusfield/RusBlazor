2e8c039e6dd403ae7318ed89d40a82746dccc107
Marius <marius@fjeld.et>
2023-07-03 15:17:11 +0200

    Footer with hover setup

---
+++ RusBlazor/Handlers/TooltipHandler.cs
@@ -0,0 +1,23 @@
+﻿namespace RusBlazor.Handlers
+{
+    public static class TooltipHandler
+    {
+        static Action<string>? _tooltipAction;
+        public static void RegisterTooltipEventCallback(Action<string> action)
+        {
+            _tooltipAction = action;
+        }
+
+        public static void DisplayTooltip(string text)
+        {
+            if (_tooltipAction != null)
+                _tooltipAction.Invoke(text);
+        }
+
+        public static void ResetTooltip()
+        {
+            if (_tooltipAction != null)
+                _tooltipAction.Invoke("");
+        }
+    }
+}
