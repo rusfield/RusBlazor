f2722c0113df0a98066f563bc79ed98747928f70
Marius <marius@fjeld.et>
2023-07-26 16:26:25 +0200

    Compare button support

---
+++ RusBlazor/Wrappers/CompareWrapper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RusBlazor.Wrappers
+{
+    public class CompareWrapper<T>
+    {
+        public T Value { get; set; } = default(T);
+
+        public CompareWrapper(T value)
+        {
+            Value = value;
+        }
+    }
+}
